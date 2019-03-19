407d88e68b49cbaa79675cbe171c48f6232e48d8
example name <example@example.com>
2019-03-20 01:49:48 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,19 @@
 void reset_handler(void)
 {
 	while (1)
-	{	func1(1,2);
+	{	
+		func0();
+		func1(1,2);
 		func2(1,2,3);
 		func3(1,2,3,4);
 	}
 	;
+}
+int func0(){
+	
+	func1(3,4);
+	return 16;
+	
 }
 int func1(int a,int b){
 	return a+b;
