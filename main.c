ca94aa327d066ed01e8effb7b8e0ea928ba9ad56
example name <example@example.com>
2019-03-19 14:18:12 +0800

    adding fumctions

--- main.c
+++ main.c
@@ -1,5 +1,22 @@
 void reset_handler(void)
 {
 	while (1)
-		;
+	{	func1(1,2);
+		func2(1,2,3);
+		func3(1,2,3,4);
+	}
+	;
 }
+int func1(int a,int b){
+	return a+b;
+}
+int func2(int a,int b,int c){
+	return c+func1(a,b);
+}
+int func3(int a,int b,int c,int d){
+	return a+func2(b,c,d);
+}
+int func_caller(){
+	return func1(4,5);
+}
+
